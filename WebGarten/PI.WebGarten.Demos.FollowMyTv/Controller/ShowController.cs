5c16512f68e472dc88022d45aa42cf59f3762570
31503-isel <nelsonalmendra@gmail.com>
2012-04-18 18:51:29 +0100

    Created HomeController, ShowsController, HomeView and ShowsView. Changed type of Seasons in class Show from List<Season> to int.

---
+++ WebGarten/PI.WebGarten.Demos.FollowMyTv/Controller/ShowController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using PI.WebGarten.Demos.FollowMyTv.View;
+using PI.WebGarten.MethodBasedCommands;
+
+namespace PI.WebGarten.Demos.FollowMyTv.Controller
+{
+    class ShowController
+    {
+        [HttpCmd(HttpMethod.Get, "/shows")]
+        public HttpResponse Get()
+        {
+            return new HttpResponse(HttpStatusCode.OK, new ShowsView(RepositoryLocator.Shows.GetAll()));
+        }
+
+        [HttpCmd(HttpMethod.Get, "/shows/{id}")]
+        public HttpResponse Get(string id)
+        {
+            return new HttpResponse(HttpStatusCode.OK, new ShowsView(RepositoryLocator.Shows.GetById(id)));
+        }
+    }
+}
---
+++ WebGarten/PI.WebGarten.Demos.FollowMyTv/View/HomeView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PI.WebGarten.HttpContent.Html;
+
+namespace PI.WebGarten.Demos.FollowMyTv.View
+{
+    class HomeView : HtmlDoc
+    {
+        public HomeView() : base("Home",
+            H1(Text("Home")),
+            Ul( Li(A(ResolveUri.ForUsers(), "Users")), Li(A(ResolveUri.ForShows(), "TV Shows")))
+            )
+        {}
+    }
+}
