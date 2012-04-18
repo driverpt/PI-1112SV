91f6165e1c5aaf1df5bced09623f948eb4d06005
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-04-18 19:11:08 +0100

    Set ProposalController visibility to Public.
    Refactored some Method names to be more Human Readable/Programmer Friendly.
    Added an Id as primary key to Proposal Model.
    Updated ResolveUri's to include the Proposal URI's.
    Fixed Compilation problems in ResolveUri for User.

--- WebGarten/PI.WebGarten.Demos.FollowMyTv/ResolveUri.cs
+++ WebGarten/PI.WebGarten.Demos.FollowMyTv/ResolveUri.cs
@@ -6,12 +6,17 @@ namespace PI.WebGarten.Demos.FollowMyTv
     {
         public static string For(User user)
         {
-            return string.Format("/user/{0}", user.Id);
+            return string.Format("/user/{0}", user.Identity.Name);
         }
 
         public static string ForUsers()
         {
             return "/users";
+        }
+
+        public static string For(Proposal proposal)
+        {
+            return string.Format("/proposals/{0}", proposal.Id);
         }
     }
 }
