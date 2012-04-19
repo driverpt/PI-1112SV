5ab27d963312262c76b73dcec1e77177f4b3fed1
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-04-19 16:02:41 +0100

    Added Authentication Controller

---
+++ WebGarten/PI.WebGarten.Demos.FollowMyTv/Controller/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using PI.WebGarten.MethodBasedCommands;
+
+namespace PI.WebGarten.Demos.FollowMyTv.Controller
+{
+    public class AuthenticationController : BaseController
+    {
+
+        [HttpCmd(HttpMethod.Get, "/login")]
+        public HttpResponse Login()
+        {
+            return null;
+        }
+
+        [HttpCmd(HttpMethod.Get, "/logout")]
+        public HttpResponse Logout()
+        {
+            return null;
+        }
+    }
+}
