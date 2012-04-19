25669442b9dbe97e5d423923aaf51f5e8a960edc
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-04-19 18:10:16 +0100

    Fixed a bug in ProposalView name.
    Added New ProposalForm.

--- WebGarten/PI.WebGarten.Demos.FollowMyTv/Controller/ProposalController.cs
+++ WebGarten/PI.WebGarten.Demos.FollowMyTv/Controller/ProposalController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using PI.WebGarten.Demos.FollowMyTv.Model;
+using PI.WebGarten.Demos.FollowMyTv.View;
 using PI.WebGarten.MethodBasedCommands;
 using PI.WebGarten.Mvc;
 
@@ -15,19 +16,18 @@ namespace PI.WebGarten.Demos.FollowMyTv.Controller
             var user = User.Identity.Name;
             IEnumerable<Proposal> proposals = RepositoryLocator.Proposals.GetAll().Where( nome => nome.User.Equals(user) );
 
-            return new HttpResponse(HttpStatusCode.OK, new ProposalsView(proposals));
+            return new HttpResponse(HttpStatusCode.OK, new ProposalView(proposals));
         }
 
         [HttpCmd( HttpMethod.Get, "/proposals/{id}" )]
         public HttpResponse GetProposal(int id)
-        {
-            return null;
+        {            return null;
         }
 
         [HttpCmd( HttpMethod.Get, "/proposals/new")]
         public HttpResponse GetNewProposalForm()
         {
-            return null;
+            return new HttpResponse(HttpStatusCode.OK, new ProposalForm());
         }
 
         [HttpCmd( HttpMethod.Post, "/proposals/new" )]
