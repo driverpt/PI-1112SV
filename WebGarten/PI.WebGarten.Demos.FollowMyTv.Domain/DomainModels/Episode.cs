118e4920838ee0ff5fb455b1d8770a2e67f8b070
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2012-04-20 20:09:42 +0100

    Episode no longer has a contructor.
    Fixed Program.cs according to the latest changes.

--- WebGarten/PI.WebGarten.Demos.FollowMyTv.Domain/DomainModels/Episode.cs
+++ WebGarten/PI.WebGarten.Demos.FollowMyTv.Domain/DomainModels/Episode.cs
@@ -2,14 +2,6 @@ namespace PI.WebGarten.Demos.FollowMyTv.Domain.DomainModels
 {
     public class Episode
     {
-        public Episode(string title, long duration, string synopsis, int score)
-        {
-            Title = title;
-            Duration = duration;
-            Synopsis = synopsis;
-            Score = score;
-        }
-
         public int Number { get; set; }
         public string Title { get; set; }
         public long Duration { get; set; }
--- WebGarten/PI.WebGarten.Demos.FollowMyTv/Program.cs
+++ WebGarten/PI.WebGarten.Demos.FollowMyTv/Program.cs
@@ -33,43 +33,33 @@ namespace PI.WebGarten.Demos.FollowMyTv
 
         static void InitShows()
         {
-            Show show = new Show("Breaking Bad", "Informed he has terminal cancer, an underachieving chemistry genius " +
+            Show show = new Show{ Name = "Breaking Bad"
+                                , Description = "Informed he has terminal cancer, an underachieving chemistry genius "  +
                                                   "turned high school chemistry teacher turns to using his expertise in " +
-                                                  "chemistry to provide a legacy for his family..." +
-                                                  " by producing the world's highest quality crystal meth.");
-            Season season = new Season("1", new DateTime(2008, 1, 28), new DateTime(2008, 3, 10));
-            Episode episode = new Episode("Pilot", 40, "Walter White, a 50-year old chemistry teacher, secretly begins making crystal methamphetamine" +
-                                                       " to support his family when he finds out that he has terminal lung cancer. He teams up with a former student," +
-                                                       " Jesse Pinkman, who is a meth dealer. Jesse is trying to sell the meth but the dealers snatch him and make him" +
-                                                       " show them the lab. Walter knows they intend to kill him so he poisons them while showing them his recipe.", 4);
+                                                  "chemistry to provide a legacy for his family..."                       +
+                                                  " by producing the world's highest quality crystal meth."
+                                };
+            Season season = new Season
+                                {  
+                                   Number = 1
+                                 , Name = "1"
+                                 , Debut = new DateTime(2008, 1, 28)
+                                 , Finale = new DateTime(2008, 3, 10)
+                                };
+            Episode episode = new Episode
+                                  {
+                                        Title = "Pilot"
+                                      , Duration = 40
+                                      , Synopsis =
+                                          "Walter White, a 50-year old chemistry teacher, secretly begins making crystal methamphetamine" +
+                                          " to support his family when he finds out that he has terminal lung cancer. He teams up with a former student," +
+                                          " Jesse Pinkman, who is a meth dealer. Jesse is trying to sell the meth but the dealers snatch him and make him" +
+                                          " show them the lab. Walter knows they intend to kill him so he poisons them while showing them his recipe."
+                                      , Score = 4
+                                  };
             ShowService.AddShow(show);
             ShowService.AddSeasonToShow(show.Name, season);
             ShowService.AddEpisodeToSeason(show.Name, 1, episode);
-
-            /*RepositoryLocator.Shows.Add(new Show("Breaking Bad"
-                                                , "Informed he has terminal cancer, an underachieving chemistry genius " +
-                                                  "turned high school chemistry teacher turns to using his expertise in " +
-                                                  "chemistry to provide a legacy for his family..." +
-                                                  " by producing the world's highest quality crystal meth."
-                                                )
-                                       );
-            Episode episode = new Episode("Pilot", 40, "Walter White, a 50-year old chemistry teacher, secretly begins making crystal methamphetamine" +
-                                                       " to support his family when he finds out that he has terminal lung cancer. He teams up with a former student," +
-                                                       " Jesse Pinkman, who is a meth dealer. Jesse is trying to sell the meth but the dealers snatch him and make him" +
-                                                       " show them the lab. Walter knows they intend to kill him so he poisons them while showing them his recipe.", 4);
-            List<Episode> episodes = new List<Episode>();
-            episodes.Add(episode);
-
-            Season season = new Season("1", new DateTime(2008, 1, 28), new DateTime(2008, 3, 10));
-            List<Season> seasons = new List<Season>();
-            seasons.Add(season);*/
-
-            /*RepositoryLocator.Shows.Add("Breaking Bad", new Show("Breaking Bad"
-                                                                , "Informed he has terminal cancer, an underachieving chemistry genius " +
-                                                                  "turned high school chemistry teacher turns to using his expertise in " +
-                                                                  "chemistry to provide a legacy for his family..." +
-                                                                  " by producing the world's highest quality crystal meth."
-                                                                , seasons));*/
         }
 
         static void Main( string[] args )
